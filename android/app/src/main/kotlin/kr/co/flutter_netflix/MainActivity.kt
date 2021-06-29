e743d8c8d2fcc755c2036f6357cac8a109c04ee4
olleh7531 <42690897+olleh7531@users.noreply.github.com>
2021-06-29 22:14:29 +0900

    bottom bar

---
+++ android/app/src/main/kotlin/kr/co/flutter_netflix/MainActivity.kt
@@ -0,0 +1,6 @@
+package kr.co.flutter_netflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "BbongFlix",
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            //physics: NeverScrollableScrollPhysics(),
+            children: [ 
+              Container(),
+              Container(),
+              Container(),
+              Container(),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
